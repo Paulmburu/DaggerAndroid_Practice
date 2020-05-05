ae712103cd5e6b2afeed55dd033eb8a2f6b93278
Paulmburu <paulmburu53@gmail.com>
2020-05-06 01:21:34 +0300

    renamed MainActivity to AuthActivity, & layout too

--- app/src/main/java/tk/paulmburu/daggerandroid_practice/AuthActivity.kt
+++ app/src/main/java/tk/paulmburu/daggerandroid_practice/AuthActivity.kt
@@ -3,10 +3,10 @@ package tk.paulmburu.daggerandroid_practice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth)
     }
 }
