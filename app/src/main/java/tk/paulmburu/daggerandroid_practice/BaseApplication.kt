8e4c06627b7a236e5c80be0737df05fa92f26795
Paulmburu <paulmburu53@gmail.com>
2020-05-06 01:00:11 +0300

    return new generated DaggerAppComponent in applicationInjector

--- app/src/main/java/tk/paulmburu/daggerandroid_practice/BaseApplication.kt
+++ app/src/main/java/tk/paulmburu/daggerandroid_practice/BaseApplication.kt
@@ -2,9 +2,10 @@ package tk.paulmburu.daggerandroid_practice
 
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import tk.paulmburu.daggerandroid_practice.di.DaggerAppComponent
 
 class BaseApplication : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication>? {
-        return null
+        return DaggerAppComponent.builder().application(this).build()
     }
 }
