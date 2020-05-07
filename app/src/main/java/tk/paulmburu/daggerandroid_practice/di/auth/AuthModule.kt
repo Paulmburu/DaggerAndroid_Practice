aeae062a01389c48d558b98c811d5fd389ce5531
Paulmburu <paulmburu53@gmail.com>
2020-05-08 00:05:25 +0300

    provide AuthApi

---
+++ app/src/main/java/tk/paulmburu/daggerandroid_practice/di/auth/AuthModule.kt
@@ -0,0 +1,16 @@
+package tk.paulmburu.daggerandroid_practice.di.auth
+
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import tk.paulmburu.daggerandroid_practice.network.auth.AuthApi
+
+@Module
+class AuthModule {
+
+    @Provides
+    fun provideAuthApi(retrofit: Retrofit): AuthApi {
+        return retrofit.create(AuthApi::class.java)
+    }
+
+}
