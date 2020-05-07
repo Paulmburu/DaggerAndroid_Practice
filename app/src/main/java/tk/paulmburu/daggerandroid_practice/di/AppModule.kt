abf67590f89572d7f5781ed798189cb49da00148
Paulmburu <paulmburu53@gmail.com>
2020-05-08 00:06:07 +0300

    provide RetrofitInstance

--- app/src/main/java/tk/paulmburu/daggerandroid_practice/di/AppModule.kt
+++ app/src/main/java/tk/paulmburu/daggerandroid_practice/di/AppModule.kt
@@ -8,11 +8,24 @@ import com.bumptech.glide.RequestManager
 import com.bumptech.glide.request.RequestOptions
 import dagger.Module
 import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import tk.paulmburu.daggerandroid_practice.R
+import tk.paulmburu.daggerandroid_practice.util.Constants
 import javax.inject.Singleton
 
 @Module
 class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
     @Singleton
     @Provides
     fun provideRequestOptions() : RequestOptions{
