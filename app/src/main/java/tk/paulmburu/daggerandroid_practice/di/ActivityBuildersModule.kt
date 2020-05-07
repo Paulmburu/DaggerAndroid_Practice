0e2c946294025465dafdfc9f5cfd3a8cb3017711
Paulmburu <paulmburu53@gmail.com>
2020-05-07 15:32:10 +0300

    update ui -> auth package

--- app/src/main/java/tk/paulmburu/daggerandroid_practice/di/ActivityBuildersModule.kt
+++ app/src/main/java/tk/paulmburu/daggerandroid_practice/di/ActivityBuildersModule.kt
@@ -2,7 +2,7 @@ package tk.paulmburu.daggerandroid_practice.di
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
-import tk.paulmburu.daggerandroid_practice.AuthActivity
+import tk.paulmburu.daggerandroid_practice.ui.auth.AuthActivity
 
 @Module
 abstract class ActivityBuildersModule {
