08fb5e193af077d7ded041bec54b7bfa734984f4
Paulmburu <paulmburu53@gmail.com>
2020-05-08 00:00:34 +0300

    add AuthModule to AuthActivity builders

--- app/src/main/java/tk/paulmburu/daggerandroid_practice/di/ActivityBuildersModule.kt
+++ app/src/main/java/tk/paulmburu/daggerandroid_practice/di/ActivityBuildersModule.kt
@@ -2,10 +2,14 @@ package tk.paulmburu.daggerandroid_practice.di
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
+import tk.paulmburu.daggerandroid_practice.di.auth.AuthModule
+import tk.paulmburu.daggerandroid_practice.di.auth.AuthViewModelModule
 import tk.paulmburu.daggerandroid_practice.ui.auth.AuthActivity
 
 @Module
 abstract class ActivityBuildersModule {
-    @ContributesAndroidInjector
+    @ContributesAndroidInjector(
+        modules = [AuthViewModelModule::class, AuthModule::class]
+    )
     abstract fun contributeAuthInjector(): AuthActivity
 }
