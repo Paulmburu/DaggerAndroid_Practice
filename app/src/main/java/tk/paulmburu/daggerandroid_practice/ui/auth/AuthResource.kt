6bfea70b252ea56ace5876de5a450c187873afd1
Paulmburu <paulmburu53@gmail.com>
2020-05-08 19:23:44 +0300

    create user status wrapper

---
+++ app/src/main/java/tk/paulmburu/daggerandroid_practice/ui/auth/AuthResource.kt
@@ -0,0 +1,30 @@
+package tk.paulmburu.daggerandroid_practice.ui.auth
+
+import androidx.annotation.NonNull
+import androidx.annotation.Nullable
+
+class AuthResource<T>(@field:NonNull @param:NonNull val status: AuthStatus, @field:Nullable @param:Nullable val data: T, @field:Nullable @param:Nullable val message: String?) {
+
+    enum class AuthStatus {
+        AUTHENTICATED, ERROR, LOADING, NOT_AUTHENTICATED
+    }
+
+    companion object {
+        fun <T> authenticated(@Nullable data: T): AuthResource<T> {
+            return AuthResource(AuthStatus.AUTHENTICATED, data, null)
+        }
+
+        fun <T> error(@NonNull msg: String?, @Nullable data: T): AuthResource<T> {
+            return AuthResource(AuthStatus.ERROR, data, msg)
+        }
+
+        fun <T> loading(@Nullable data: T): AuthResource<T> {
+            return AuthResource(AuthStatus.LOADING, data, null)
+        }
+
+        fun <T> logout(): AuthResource<Any?> {
+            return AuthResource<Any?>(AuthStatus.NOT_AUTHENTICATED, null, null)
+        }
+    }
+
+}
