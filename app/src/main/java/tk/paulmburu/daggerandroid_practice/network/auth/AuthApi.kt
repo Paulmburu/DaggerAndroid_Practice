bae23890150ae2cb5f13324947a36ae44ddf97c1
Paulmburu <paulmburu53@gmail.com>
2020-05-08 09:05:09 +0300

    get Flowable data object (rxjava)

--- app/src/main/java/tk/paulmburu/daggerandroid_practice/network/auth/AuthApi.kt
+++ app/src/main/java/tk/paulmburu/daggerandroid_practice/network/auth/AuthApi.kt
@@ -1,11 +1,14 @@
 package tk.paulmburu.daggerandroid_practice.network.auth
 
+import io.reactivex.Flowable
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
+import tk.paulmburu.daggerandroid_practice.models.User
 
 
 interface AuthApi {
-    @GET
-    fun getFakeStuff(): Call<ResponseBody>
+    @GET("users/{id}")
+    fun getUser(@Path("id") id : Int): Flowable<User>
 }
