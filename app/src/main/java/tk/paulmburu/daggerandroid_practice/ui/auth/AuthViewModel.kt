a4b86803c776cdbeb56c48b71908c458048abb73
Paulmburu <paulmburu53@gmail.com>
2020-05-08 09:06:08 +0300

    observe & read AuthApi data

--- app/src/main/java/tk/paulmburu/daggerandroid_practice/ui/auth/AuthViewModel.kt
+++ app/src/main/java/tk/paulmburu/daggerandroid_practice/ui/auth/AuthViewModel.kt
@@ -1,21 +1,35 @@
 package tk.paulmburu.daggerandroid_practice.ui.auth
 
 import android.util.Log
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
+import io.reactivex.schedulers.Schedulers
+import tk.paulmburu.daggerandroid_practice.models.User
 import tk.paulmburu.daggerandroid_practice.network.auth.AuthApi
 import javax.inject.Inject
 
 class AuthViewModel @Inject constructor(authApi: AuthApi) : ViewModel() {
     val TAG = "AuthViewModel"
-    var thisApi = authApi
+    var thisAuthApi = authApi
 
 
     init {
         Log.d(TAG,"AuthViewModel: viewmodel is working")
+        getUser()
+    }
+
+    fun getUser(){
+        Log.d(TAG,"AuthViewModel: get user")
+        thisAuthApi.getUser(1)
+            .toObservable()
+            .subscribeOn(Schedulers.io())
+            .subscribe{
+                Log.d(TAG,"onNext =  ${it.email}")
+            }
     }
 
     fun checkAuthApiNull(){
-        if (thisApi == null){
+        if (thisAuthApi == null){
             Log.d(TAG,"AuthViewModel: auth api is NULL")
         }else{
             Log.d(TAG,"AuthViewModel: auth api is NOT NULL")
