2bc90d29d93b6088cc86181ef00b400904bfdf1d
Paulmburu <paulmburu53@gmail.com>
2020-05-11 13:03:11 +0300

    SessionManager->single source of truth for User data

---
+++ app/src/main/java/tk/paulmburu/daggerandroid_practice/SessionManager.kt
@@ -0,0 +1,36 @@
+package tk.paulmburu.daggerandroid_practice
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.Observer
+import tk.paulmburu.daggerandroid_practice.models.User
+import tk.paulmburu.daggerandroid_practice.ui.auth.AuthResource
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SessionManager @Inject constructor() {
+    val TAG: String = "SessionManager"
+
+    var cachedUser: MediatorLiveData<AuthResource<User>>  = MediatorLiveData()
+
+    fun authenticateWithId(source: LiveData<AuthResource<User>>){
+        if(cachedUser != null){
+            cachedUser.value = AuthResource.loading(User(0,"","",""))
+            cachedUser.addSource(source, Observer{userAuthResource ->
+                cachedUser.value =  userAuthResource
+                cachedUser.removeSource(source)
+            })
+        }
+    }
+
+    fun logout(){
+        Log.d(TAG,"logOut: logging out")
+        cachedUser.value = AuthResource.logout<User>()
+    }
+
+    fun getAuthUser(): LiveData<AuthResource<User>>{
+        return cachedUser
+    }
+}
