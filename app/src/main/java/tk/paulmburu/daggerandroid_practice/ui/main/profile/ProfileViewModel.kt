e64cc2f09897299fc93034c5082cf7bcef92c2c3
Paulmburu <paulmburu53@gmail.com>
2020-05-13 14:50:13 +0300

    Profile ViewModel

---
+++ app/src/main/java/tk/paulmburu/daggerandroid_practice/ui/main/profile/ProfileViewModel.kt
@@ -0,0 +1,14 @@
+package tk.paulmburu.daggerandroid_practice.ui.main.profile
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class ProfileViewModel @Inject constructor(): ViewModel() {
+    val TAG = "ProfileViewModel"
+
+    init {
+        Log.d(TAG,"ProfileViewModel: viewmodel is ready... ")
+    }
+
+}
