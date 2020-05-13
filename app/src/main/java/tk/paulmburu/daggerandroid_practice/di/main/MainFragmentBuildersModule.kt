bdc14480561f7d8384ff4dcff6623ec43979468e
Paulmburu <paulmburu53@gmail.com>
2020-05-13 20:48:19 +0300

    addd PostsFragment to Dagger

--- app/src/main/java/tk/paulmburu/daggerandroid_practice/di/main/MainFragmentBuildersModule.kt
+++ app/src/main/java/tk/paulmburu/daggerandroid_practice/di/main/MainFragmentBuildersModule.kt
@@ -2,6 +2,7 @@ package tk.paulmburu.daggerandroid_practice.di.main
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
+import tk.paulmburu.daggerandroid_practice.ui.main.posts.PostsFragment
 import tk.paulmburu.daggerandroid_practice.ui.main.profile.ProfileFragment
 
 @Module
@@ -9,4 +10,7 @@ abstract class MainFragmentBuildersModule {
 
     @ContributesAndroidInjector
     abstract fun contributeProfileFragment(): ProfileFragment
+
+    @ContributesAndroidInjector
+    abstract fun contributePostsFragment(): PostsFragment
 }
