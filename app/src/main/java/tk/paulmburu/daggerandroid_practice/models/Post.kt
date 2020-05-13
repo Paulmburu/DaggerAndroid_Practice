d68a236bd0bf646ead0402c0c2044a2cb7e49acc
Paulmburu <paulmburu53@gmail.com>
2020-05-13 16:57:37 +0300

    create Post data class

---
+++ app/src/main/java/tk/paulmburu/daggerandroid_practice/models/Post.kt
@@ -0,0 +1,23 @@
+package tk.paulmburu.daggerandroid_practice.models
+
+import com.google.gson.annotations.Expose
+
+import com.google.gson.annotations.SerializedName
+
+data class Post(
+    @SerializedName("userId")
+    @Expose
+    var userId: Int = 0,
+
+    @SerializedName("id")
+    @Expose
+    var id: Int = 0,
+
+    @SerializedName("title")
+    @Expose
+    var title: String? = null,
+
+    @SerializedName("body")
+    @Expose
+    var body: String? = null
+)
