e9a7daced6abbd764ac39f9b70ac8ca3f765156f
Paulmburu <paulmburu53@gmail.com>
2020-05-13 15:46:23 +0300

    update view wdigets on-data change

--- app/src/main/java/tk/paulmburu/daggerandroid_practice/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/tk/paulmburu/daggerandroid_practice/ui/main/profile/ProfileFragment.kt
@@ -5,10 +5,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import dagger.android.support.DaggerFragment
 import tk.paulmburu.daggerandroid_practice.R
+import tk.paulmburu.daggerandroid_practice.models.User
+import tk.paulmburu.daggerandroid_practice.ui.auth.AuthResource
 import tk.paulmburu.daggerandroid_practice.viewmodels.ViewModelProviderFactory
 import javax.inject.Inject
 
@@ -16,6 +20,9 @@ class ProfileFragment : DaggerFragment() {
     val TAG = "ProfileFragment"
 
     lateinit var viewModel: ProfileViewModel
+    lateinit var email :TextView
+    lateinit var username :TextView
+    lateinit var website :TextView
 
     @Inject
     lateinit var providerFactory: ViewModelProviderFactory
@@ -26,6 +33,7 @@ class ProfileFragment : DaggerFragment() {
         savedInstanceState: Bundle?
     ): View? {
         Toast.makeText(activity,"Profile Fragment created",Toast.LENGTH_LONG).show()
+
         return inflater.inflate(R.layout.fragment_profile,container,false)
     }
 
@@ -34,8 +42,40 @@ class ProfileFragment : DaggerFragment() {
 //        super.onViewCreated(view, savedInstanceState)
         Log.d(TAG,"onViewCreated: ProfileFragment was created ...")
 
+        email = view!!.findViewById(R.id.email)
+        username = view!!.findViewById(R.id.username)
+        website = view!!.findViewById(R.id.website)
+
         viewModel = ViewModelProviders.of(this, providerFactory).get(ProfileViewModel::class.java)
+        subscribeObservers()
     }
 
+    fun subscribeObservers(){
+        viewModel.getAuthenticateUser().removeObservers(viewLifecycleOwner)
+        viewModel.getAuthenticateUser().observe(viewLifecycleOwner, Observer<AuthResource<User>>(){
+            if(it != null){
+                when(it.status){
+                    AuthResource.AuthStatus.AUTHENTICATED -> {
+                        setUserDetails(it.data!!)
+                    }
+                    AuthResource.AuthStatus.ERROR -> {
+                        setErrorDetails(it.message)
+                    }
+                }
+            }
+        })
+    }
+
+    private fun setUserDetails(data: User) {
+        email.setText(data.email)
+        username.setText(data.username)
+        website.setText(data.website)
+    }
+
+    private fun setErrorDetails(message: String?) {
+        email.setText(message)
+        username.setText("Error")
+        website.setText("Error")
+    }
 
 }
