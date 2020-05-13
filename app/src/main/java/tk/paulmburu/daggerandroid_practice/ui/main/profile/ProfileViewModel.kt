e818b2622fc4ca16d1834dffb0f8cdd94de444ae
Paulmburu <paulmburu53@gmail.com>
2020-05-13 15:45:58 +0300

    Inject and Observe SessionManager

--- app/src/main/java/tk/paulmburu/daggerandroid_practice/ui/main/profile/ProfileViewModel.kt
+++ app/src/main/java/tk/paulmburu/daggerandroid_practice/ui/main/profile/ProfileViewModel.kt
@@ -1,14 +1,23 @@
 package tk.paulmburu.daggerandroid_practice.ui.main.profile
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import tk.paulmburu.daggerandroid_practice.SessionManager
+import tk.paulmburu.daggerandroid_practice.models.User
+import tk.paulmburu.daggerandroid_practice.ui.auth.AuthResource
 import javax.inject.Inject
 
-class ProfileViewModel @Inject constructor(): ViewModel() {
+class ProfileViewModel @Inject constructor(sessionManager: SessionManager): ViewModel() {
     val TAG = "ProfileViewModel"
+    var sessionManager = sessionManager
 
     init {
         Log.d(TAG,"ProfileViewModel: viewmodel is ready... ")
+    }
+
+    fun getAuthenticateUser(): LiveData<AuthResource<User>>{
+        return sessionManager.getAuthUser()
     }
 
 }
