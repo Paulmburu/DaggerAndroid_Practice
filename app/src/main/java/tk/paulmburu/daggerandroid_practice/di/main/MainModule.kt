d19b1f51ecc7c8f24bff96fdf35d0da4ac492a3f
Paulmburu <paulmburu53@gmail.com>
2020-05-14 12:07:08 +0300

    let dagger provide adapter dependency

--- app/src/main/java/tk/paulmburu/daggerandroid_practice/di/main/MainModule.kt
+++ app/src/main/java/tk/paulmburu/daggerandroid_practice/di/main/MainModule.kt
@@ -4,10 +4,16 @@ import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
 import tk.paulmburu.daggerandroid_practice.network.main.MainApi
+import tk.paulmburu.daggerandroid_practice.ui.main.posts.PostsRecyclerAdapter
 
 
 @Module
 class MainModule {
+
+    @Provides
+    fun provideAdapter():PostsRecyclerAdapter{
+        return PostsRecyclerAdapter()
+    }
 
     @Provides
     fun provideMainApi(retrofit: Retrofit): MainApi {
